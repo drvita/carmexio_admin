abf91beccbe8e50f05b7e74ba4913baa8a55b829
drvita <chavagalindo@live.com.mx>
2023-07-05 20:54:33 -0600

    Updates

---
+++ _nuxt/loged.071b6146.js
@@ -0,0 +1,1 @@
+import{J as e,n as o}from"./entry.2c6d6405.js";import{u as t}from"./useStorage.bacf4001.js";const d=e(()=>{if(t().getToken())return o("/dashboard")});export{d as default};
--- _nuxt/loged.470f632c.js
+++
@@ -1,1 +0,0 @@
-import{J as e,n as o}from"./entry.1d58e583.js";import{u as t}from"./useStorage.b4f7786c.js";const d=e(()=>{if(t().getToken())return o("/dashboard")});export{d as default};
